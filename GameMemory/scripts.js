b707c550d16cec6c8ddf60e1d8f5634f6e7644f6
ClaudiaNV <58890912+ClaudiaNV@users.noreply.github.com>
2020-01-21 17:00:23 +0000

    changed array

--- GameMemory/scripts.js
+++ GameMemory/scripts.js
@@ -1,4 +1,4 @@
-//array card
+//array cards
 const test =[
   {imgf:"../img1.png", altf: "Mouse", imgb:"../logo.png", altb:"Meromy"},
   {imgf:"../img1.png", altf: "Mouse", imgb:"../logo.png", altb:"Meromy"},
